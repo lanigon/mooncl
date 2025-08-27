2f46b4396e900c2b1fbdbfd8bcaa0444c1593076
bergtatt <756004757@qq.com>
2025-08-27 18:01:27 +0800

    fsd

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <WalletProvider>
+          {children}
+        </WalletProvider>
       </body>
     </html>
   );
